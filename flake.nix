c0eb643a48024dc20c7434bda451a33a8f227fb9
g <gtmalmgren@gmail.com>
2023-01-18 09:37:59 -0800

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Nix packaging for amazon-ion/ion-cli";
+
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-22.11";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+
+        ion-cli = pkgs.rustPlatform.buildRustPackage {
+          pname = "ion";
+          version = "0.6.0";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+          doCheck = false;
+        };
+      in
+      {
+        packages.default = ion-cli;
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = ion-cli.buildInputs;
+          # buildRustPackage defines the baseline native build inputs
+          nativeBuildInputs = ion-cli.nativeBuildInputs;
+        };
+      });
+}
